d509adbe3fdb08298666cd4198c8d799f197e9ea
vitoria74 <maria_vitoria74@hotmail.com>
2024-07-11 15:59:51 -0300

    entidades do bd na classe Trip

---
+++ planner/src/main/java/com/rocketseat/planner/trip/Trip.java
@@ -0,0 +1,37 @@
+package com.rocketseat.planner.trip;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity //indca para o sping a pro jpa que essa classe é uma entidade
+public class Trip {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) //indica que foi gerada automanticamente
+    private UUID id;
+
+    @Column(nullable = false)
+    private String destination;
+
+    @Column(name = "starts_at", nullable = false)
+    private LocalDateTime startsAt;
+
+    @Column(name = "ends_at", nullable = false)
+    private LocalDateTime endsAt;
+
+    @Column(name = "is_confirmed", nullable = false)
+    private Boolean isConfirmed;
+
+    @Column(name = "owner_name", nullable = false)
+    private String ownerName;
+
+    @Column(name = "owner_email", nullable = false)
+    private String ownerEmail;
+}
